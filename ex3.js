faa029b0e12a972944709ad417b5853515871ae8
kugatheesan <vkugatheesan@gmail.com>
2024-09-27 15:41:45 +0530

    first commit

---
+++ ex3.js
@@ -0,0 +1,40 @@
+// // function Hello(){
+// //     var para=document.getElementById("para");
+// //     para.innerHTML="Lion is a Always Lion ";
+// // }
+
+// document.getElementById("but").addEventListener("click", changeParagraphText);
+function changeParagraphText() {
+    document.getElementById("textParagraph").innerHTML = "The paragraph text has been changed!";
+}        function changeDivColor() {
+    document.getElementById("colorDiv").style.backgroundColor = "blue";
+}        function toggleParagraph() {
+    const paragraph = document.getElementById("toggleParagraph");
+    if (paragraph.style.display === "none") 
+        {
+        paragraph.style.display = "block";
+    } else {
+        paragraph.style.display = "none";
+    }
+}        function addListItem() {
+    const newItem = document.createElement("li");
+    newItem.textContent = "New Item";
+    document.getElementById("myList").appendChild(newItem);
+}        function changeImageSource() {
+    document.getElementById("myImage").src = "Untitled.jpeg";
+}        function countParagraphs() {
+    const paragraphs = document.getElementsByTagName("p");
+    alert("Number of paragraphs: " + paragraphs.length);
+}        function deleteListItem() {
+    const list = document.getElementById("myList");
+    if (list.lastChild) {
+        list.removeChild(list.lastChild);
+    }
+}
+
+
+// function changeParagraphText() {
+//     var para = document.getElementById("textParagraph");
+//     para.innerHTML = "Lion is a Always Lion";
+// }
+
